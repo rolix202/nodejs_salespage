f8124cf68d7d89803ccca978a39e22813129b811
rolix202 <williamsroland147@gmail.com>
2024-01-15 19:36:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+const app = express()
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+dotenv.config()
+import path from "path"
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { renderFile } from "ejs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.static(__dirname + "/public/"));
+
+app.engine('ejs', renderFile);
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.render('homepage/includes.ejs')
+})
+app.get('/contact', (req, res) => {
+    res.render('contact/includes.ejs')
+})
+app.get('/about', (req, res) => {
+    res.render('about/includes.ejs')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server running on PORT ${port}`);
+})
